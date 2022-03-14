149f87e29db7908e5bdb68c1985afce50b16f197
theopmw <theowrightdev@gmail.com>
2022-03-14 19:13:11 +0000

    Add responsive Bootstrap cards to display placeholder images and titles

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,12 @@
 $.getJSON("/placeholders.json", function (data) {
-  // alert(data.length);
+  let card = "";
   for (let i = 0; i < 20; i++) {
-    console.log(data[i]);
+    card +=
+      '<div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center"> <div class="card m-3"> <img class="card-img-top" src="' +
+      data[i].thumbnailUrl +
+      '" alt="Card image cap"> <div class="card-body"> <h5 class="card-title">' +
+      data[i].title +
+      "</h5> </div> </div> </div>";
+    document.getElementById("card").innerHTML = card;
   }
 });
