289e6fc1187dbbdbf73884a8870007e81458b295
theopmw <theowrightdev@gmail.com>
2022-03-15 14:56:42 +0000

    Remove console.logs and commented out code

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,3 @@
-// $.ajaxSetup({
-//   scriptCharset: "utf-8",
-//   contentType: "application/json; charset=utf-8",
-// });
-
-// $.getJSON("/placeholders.json", function (data) {
-//   let card = "";
-//   for (let i = 0; i < 20; i++) {
-//     card +=
-//       '<div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center"> <div class="card m-4"> <img class="card-img-top" src="' +
-//       data[i].thumbnailUrl +
-//       '" alt="Card image cap"> <div class="card-body"> <h5 class="card-title">' +
-//       data[i].title +
-//       "</h5> </div> </div> </div>";
-//     document.getElementById("card").innerHTML = card;
-//   }
-// });
-
 $(function () {
   $.ajax({
     dataType: "json",
@@ -23,9 +5,7 @@ $(function () {
     url: "candyspace/static/js/placeholders.json",
     success: function (data) {
       let card = "";
-      console.log(data);
       for (let i = 0; i < 20; i++) {
-        console.log(data);
         card +=
           '<div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center"> <div class="card m-4"> <img class="card-img-top" src="' +
           data[i].thumbnailUrl +
