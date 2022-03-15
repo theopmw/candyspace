f21b9ae088903fbfcd52f5037ede8b7b2cf9f1aa
theopmw <theowrightdev@gmail.com>
2022-03-15 09:56:48 +0000

    Increase card spacing

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@ $.getJSON("/placeholders.json", function (data) {
   let card = "";
   for (let i = 0; i < 20; i++) {
     card +=
-      '<div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center"> <div class="card m-3"> <img class="card-img-top" src="' +
+      '<div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center"> <div class="card m-4"> <img class="card-img-top" src="' +
       data[i].thumbnailUrl +
       '" alt="Card image cap"> <div class="card-body"> <h5 class="card-title">' +
       data[i].title +
