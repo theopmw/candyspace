933c0a62c77528cae25bbca9dee4d000326885d7
theopmw <theowrightdev@gmail.com>
2022-03-15 11:46:03 +0000

    Use ajax to pull json data

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,35 @@
-$.getJSON("../placeholders.json", function (data) {
-  let card = "";
-  for (let i = 0; i < 20; i++) {
-    card +=
-      '<div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center"> <div class="card m-4"> <img class="card-img-top" src="' +
-      data[i].thumbnailUrl +
-      '" alt="Card image cap"> <div class="card-body"> <h5 class="card-title">' +
-      data[i].title +
-      "</h5> </div> </div> </div>";
-    document.getElementById("card").innerHTML = card;
-  }
+// $.ajaxSetup({
+//   scriptCharset: "utf-8",
+//   contentType: "application/json; charset=utf-8",
+// });
+
+// $.getJSON("../placeholders.json", function (data) {
+//   let card = "";
+//   for (let i = 0; i < 20; i++) {
+//     card +=
+//       '<div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center"> <div class="card m-4"> <img class="card-img-top" src="' +
+//       data[i].thumbnailUrl +
+//       '" alt="Card image cap"> <div class="card-body"> <h5 class="card-title">' +
+//       data[i].title +
+//       "</h5> </div> </div> </div>";
+//     document.getElementById("card").innerHTML = card;
+//   }
+// });
+
+$.ajax({
+  type: "GET",
+  dataType: "json",
+  url: "../placeholders.json",
+  success: function (data) {
+    let card = "";
+    for (let i = 0; i < 20; i++) {
+      card +=
+        '<div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center"> <div class="card m-4"> <img class="card-img-top" src="' +
+        data[i].thumbnailUrl +
+        '" alt="Card image cap"> <div class="card-body"> <h5 class="card-title">' +
+        data[i].title +
+        "</h5> </div> </div> </div>";
+      document.getElementById("card").innerHTML = card;
+    }
+  },
 });
