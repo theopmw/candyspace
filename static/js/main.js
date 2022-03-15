c6d11bd3e17c11b9ed961098f43c3c1ee70394ea
theopmw <theowrightdev@gmail.com>
2022-03-15 12:52:23 +0000

    Remove function around ajax call

--- static/js/main.js
+++ static/js/main.js
@@ -16,23 +16,21 @@
 //   }
 // });
 
-$(function () {
-  $.ajax({
-    type: "GET",
-    dataType: "jsonp",
-    contentType: "application/json",
-    url: "/placeholders.json",
-    success: function (data) {
-      let card = "";
-      for (let i = 0; i < 20; i++) {
-        card +=
-          '<div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center"> <div class="card m-4"> <img class="card-img-top" src="' +
-          data[i].thumbnailUrl +
-          '" alt="Card image cap"> <div class="card-body"> <h5 class="card-title">' +
-          data[i].title +
-          "</h5> </div> </div> </div>";
-        document.getElementById("card").innerHTML = card;
-      }
-    },
-  });
+$.ajax({
+  type: "GET",
+  dataType: "jsonp",
+  contentType: "application/json",
+  url: "/placeholders.json",
+  success: function (data) {
+    let card = "";
+    for (let i = 0; i < 20; i++) {
+      card +=
+        '<div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center"> <div class="card m-4"> <img class="card-img-top" src="' +
+        data[i].thumbnailUrl +
+        '" alt="Card image cap"> <div class="card-body"> <h5 class="card-title">' +
+        data[i].title +
+        "</h5> </div> </div> </div>";
+      document.getElementById("card").innerHTML = card;
+    }
+  },
 });
